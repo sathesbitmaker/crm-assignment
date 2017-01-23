019612d834df7cfd7aa4bb9d1ba3abdd6ee0fa35
Sathes Balasingam <sathes352@gmail.com>
2017-01-23 17:00:52 -0500

    po

--- contact.rb
+++ contact.rb
@@ -1,11 +1,32 @@
 class Contact
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :email, :note
+
+  @@contacts = []
+  @@id = 1
+
+  def self.create(first_name, last_name, email, note)
+    new_contact = Contact.new(first_name, last_name, email, note)
+    @@contacts << new_contact
+    return new_contact
+  end
 
   # This method should initialize the contact's attributes
-  def initialize
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    @id = @@id
+    @@id +=1
 
   end
 
-  # This method should call the initializer, 
+#  contact = Contact.new('Betty', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
+
+end
+
+  # This method should call the initializer,
   # store the newly created contact, and then return it
   def self.create
 
@@ -22,7 +43,7 @@ class Contact
 
   end
 
-  # This method should allow you to specify 
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
@@ -54,5 +75,3 @@ class Contact
   end
 
   # Feel free to add other methods here, if you need them.
-  
-end
